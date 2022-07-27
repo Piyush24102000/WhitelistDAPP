64055f72bd53a51d67268af14f8ad8866659fca2
PiyushTale <64774043+Piyush24102000@users.noreply.github.com>
2022-07-27 22:52:22 +0530

    Update index.js

--- whitelist/pages/index.js
+++ whitelist/pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
-import { address, abi } from "./constants";
+import { address, abi } from "../constants";
 export default function Home() {
 
   const web3ModalRef = useRef();
